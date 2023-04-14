74d0ee6089c0835c9db54c2940aed7a1f97d0cfb
Jonathan Lopes <63373963+jonathan-lopes@users.noreply.github.com>
2023-04-13 22:49:20 -0300

    exercise 02 solved

--- 02/scripts.ts
+++ 02/scripts.ts
@@ -1,21 +1,36 @@
 type Item = {
-    nome: string,
-    descricao: string,
-    valor: number
-}
+  nome: string;
+  descricao: string;
+  valor: number;
+};
 
 type Cartao = {
-    numero: number,
-    validade: string,
-    nome: string,
-    cvv: number
-}
+  numero: number;
+  validade: string;
+  nome: string;
+  cvv: number;
+};
 
 type Carrinho = {
-    item: Item,
-    qtd: number,
-    desconto: number,
-    frete: number,
-    tipoTransacao: 'credito' | 'Debito',
-    cartao: Cartao
-}
+  item: Item;
+  qtd: number;
+  desconto: number;
+  frete: number;
+  tipoTransacao: "credito" | "Debito";
+  cartao: Cartao;
+};
+
+type Endereco = {
+  cep: string;
+  street: string;
+  district: string;
+  city: string;
+  state: string;
+};
+
+type Transacao = Lowercase<"credito" | "debito">;
+
+type NovoCarrinho = Omit<Carrinho, "tipoTransacao"> & {
+  endereco: Endereco;
+  tipoTransacao: Transacao;
+};
